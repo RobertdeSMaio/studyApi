f634c3c371e3f3f1fd59a7f9a206be858c92a83e
Robert <robertmaio@outlook.com>
2024-09-17 16:32:43 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using API.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Add services to the container
+builder.Services.AddDbContext<AgendaContext>(options=>
+options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));//conexao com o sql
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+//Configure the HTTP request pipeline.
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
